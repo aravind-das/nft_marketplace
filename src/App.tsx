3f8a5fba6e813440e6fec4347e3053fbc60c4984
Aravind Dasarathy <dasarathy.ar@northeastern.edu>
2024-07-14 22:33:59 +0530

    including wallet in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { wagmiConfig } from './configs';
+import ConnectWallet from './components/Wallet';
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div>
-      <header>
-        <p>Hello world</p>
-      </header>
-    </div>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <div>Hello World</div>
+          <ConnectWallet />
+        </div>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
