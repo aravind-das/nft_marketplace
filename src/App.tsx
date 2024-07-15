b73c404c95a196eae0f2b71dca226665493dd4e2
Aravind Dasarathy <dasarathy.ar@northeastern.edu>
2024-07-15 10:36:06 +0530

    using form component in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@ import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { wagmiConfig } from './configs';
 import ConnectWallet from './components/Wallet';
+import UploadImageForm from './components/UploadImageForm';
 
 const queryClient = new QueryClient();
+
+interface UploadImageFormValues {
+  name: string;
+  cost: number;
+  url: string;
+}
+
+const handleSubmit = (args: UploadImageFormValues) => {
+  // args prints the submitted form values
+  console.log(args);
+}
 
 function App() {
   return (
@@ -12,6 +24,7 @@ function App() {
         <div className="App">
           <div>Hello World</div>
           <ConnectWallet />
+          <UploadImageForm onSubmit={handleSubmit}/>
         </div>
       </QueryClientProvider>
     </WagmiProvider>
