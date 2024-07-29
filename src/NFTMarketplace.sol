7a844f70b88cdcca8e88daa9de6e3e031586ea12
Aravind Dasarathy <dasarathy.ar@northeastern.edu>
2024-07-29 16:04:20 -0400

    code for listing erc1155 tokens

--- src/NFTMarketplace.sol
+++ src/NFTMarketplace.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./PaymentHandler.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 contract NFTMarketplace is ERC721URIStorage, Ownable {
     uint256 public tokenCounter;
@@ -17,7 +18,20 @@ contract NFTMarketplace is ERC721URIStorage, Ownable {
         uint256 price;
     }
 
+    struct ERC1155Item {
+        uint256 tokenId;
+        address owner;
+        address tokenAddress;
+        uint256 amount;
+        uint256 price;
+    }
+
     mapping(uint256 => NFTItem) public nftItems;
+    mapping(uint256 => ERC1155Item) public erc1155Items;
+    uint256[] public erc1155TokenIds;
+
+    event NFTPurchased(uint256 indexed tokenId, address buyer, uint256 amount);
+    event ERC1155Purchased(uint256 indexed tokenId, address buyer, uint256 amount);
 
     constructor(string memory name, string memory symbol, address owner, address paymentHandlerAddress)
         ERC721(name, symbol)
@@ -47,25 +61,69 @@ contract NFTMarketplace is ERC721URIStorage, Ownable {
         return newItemId;
     }
 
-    function fetchAllNFTs() public view returns (NFTItem[] memory) {
+    function listERC1155(address tokenAddress, uint256 tokenId, uint256 amount, uint256 price) public onlyOwner {
+        require(tokenAddress != address(0), "Invalid token address");
+        require(amount > 0, "Amount must be greater than zero");
+
+        erc1155Items[tokenId] = ERC1155Item({
+            tokenId: tokenId,
+            owner: msg.sender,
+            tokenAddress: tokenAddress,
+            amount: amount,
+            price: price
+        });
+
+        erc1155TokenIds.push(tokenId);
+    }
+
+    function fetchAllNFTs() public view returns (NFTItem[] memory, ERC1155Item[] memory) {
         NFTItem[] memory items = new NFTItem[](tokenCounter);
         for (uint256 i = 0; i < tokenCounter; i++) {
             items[i] = nftItems[i];
         }
-        return items;
+
+        ERC1155Item[] memory erc1155ItemArray = new ERC1155Item[](erc1155TokenIds.length);
+        for (uint256 i = 0; i < erc1155TokenIds.length; i++) {
+            uint256 id = erc1155TokenIds[i];
+            erc1155ItemArray[i] = erc1155Items[id];
+        }
+
+        return (items, erc1155ItemArray);
     }
 
     function purchaseNFT(uint256 tokenId) public {
         NFTItem memory item = nftItems[tokenId];
-        // Commenting out the line to allow owner to purchase their own NFT for testing
-        // require(msg.sender != item.owner, "Owner cannot purchase their own NFT");
+        require(item.owner != msg.sender, "Owner cannot purchase their own NFT");
 
-        // Pass the creator's address (owner of the NFT) as the recipient of the payment
         paymentHandler.processPayment(tokenId, msg.sender, item.owner);
         _transfer(item.owner, msg.sender, tokenId);
 
-        // Update the owner of the NFT
         item.owner = msg.sender;
         nftItems[tokenId] = item;
+
+        emit NFTPurchased(tokenId, msg.sender, 1);
+    }
+
+    function purchaseERC1155(uint256 tokenId, uint256 amount) public payable {
+        ERC1155Item memory item = erc1155Items[tokenId];
+        require(item.owner != address(0), "Item does not exist");
+        require(amount > 0 && amount <= item.amount, "Invalid amount");
+        uint256 totalPrice = item.price * amount;
+        require(msg.value >= totalPrice, "Insufficient ETH sent");
+
+        IERC1155(item.tokenAddress).safeTransferFrom(item.owner, msg.sender, tokenId, amount, "");
+
+        if (msg.value > totalPrice) {
+            payable(msg.sender).transfer(msg.value - totalPrice);
+        }
+
+        item.amount -= amount;
+        if (item.amount == 0) {
+            delete erc1155Items[tokenId];
+        } else {
+            erc1155Items[tokenId] = item;
+        }
+
+        emit ERC1155Purchased(tokenId, msg.sender, amount);
     }
 }
